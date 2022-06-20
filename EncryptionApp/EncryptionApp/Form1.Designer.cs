10e4ec8fa436553cd28eab0201bbea4e1aa93657
CEH-101 <69151094+Gelreece@users.noreply.github.com>
2022-06-20 23:53:00 +0200

    Add files via upload

---
+++ EncryptionApp/EncryptionApp/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿
+namespace EncryptionApp
+{
+    partial class frmEcryptapp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.lblKey = new System.Windows.Forms.Label();
+            this.rtbInput = new System.Windows.Forms.RichTextBox();
+            this.nudKey = new System.Windows.Forms.NumericUpDown();
+            this.rtbOutput = new System.Windows.Forms.RichTextBox();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.lblInput = new System.Windows.Forms.Label();
+            this.lblOutput = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nudKey)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnEncrypt.Location = new System.Drawing.Point(15, 280);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(164, 35);
+            this.btnEncrypt.TabIndex = 0;
+            this.btnEncrypt.Text = "Encrypt Message";
+            this.btnEncrypt.UseVisualStyleBackColor = false;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // lblKey
+            // 
+            this.lblKey.AutoSize = true;
+            this.lblKey.Location = new System.Drawing.Point(12, 21);
+            this.lblKey.Name = "lblKey";
+            this.lblKey.Size = new System.Drawing.Size(138, 17);
+            this.lblKey.TabIndex = 1;
+            this.lblKey.Text = "Enter encryption key";
+            // 
+            // rtbInput
+            // 
+            this.rtbInput.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.rtbInput.Location = new System.Drawing.Point(15, 142);
+            this.rtbInput.Name = "rtbInput";
+            this.rtbInput.Size = new System.Drawing.Size(336, 121);
+            this.rtbInput.TabIndex = 2;
+            this.rtbInput.Text = "";
+            // 
+            // nudKey
+            // 
+            this.nudKey.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.nudKey.Location = new System.Drawing.Point(13, 52);
+            this.nudKey.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudKey.Name = "nudKey";
+            this.nudKey.Size = new System.Drawing.Size(120, 22);
+            this.nudKey.TabIndex = 3;
+            this.nudKey.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // rtbOutput
+            // 
+            this.rtbOutput.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.rtbOutput.Location = new System.Drawing.Point(3, 386);
+            this.rtbOutput.Name = "rtbOutput";
+            this.rtbOutput.Size = new System.Drawing.Size(336, 111);
+            this.rtbOutput.TabIndex = 4;
+            this.rtbOutput.Text = "";
+            this.rtbOutput.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnDecrypt.Location = new System.Drawing.Point(12, 524);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(143, 38);
+            this.btnDecrypt.TabIndex = 5;
+            this.btnDecrypt.Text = "Decrypt Message";
+            this.btnDecrypt.UseVisualStyleBackColor = false;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnCancel.Location = new System.Drawing.Point(280, 578);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(88, 30);
+            this.btnCancel.TabIndex = 6;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // lblInput
+            // 
+            this.lblInput.AutoSize = true;
+            this.lblInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInput.Location = new System.Drawing.Point(12, 106);
+            this.lblInput.Name = "lblInput";
+            this.lblInput.Size = new System.Drawing.Size(241, 17);
+            this.lblInput.TabIndex = 7;
+            this.lblInput.Text = "Enter message you\'d like to encrypt: ";
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOutput.Location = new System.Drawing.Point(27, 345);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(221, 17);
+            this.lblOutput.TabIndex = 8;
+            this.lblOutput.Text = "Here is your decrypted message: ";
+            // 
+            // frmEcryptapp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(429, 643);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.lblInput);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.rtbOutput);
+            this.Controls.Add(this.nudKey);
+            this.Controls.Add(this.rtbInput);
+            this.Controls.Add(this.lblKey);
+            this.Controls.Add(this.btnEncrypt);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmEcryptapp";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Message Encryption";
+            ((System.ComponentModel.ISupportInitialize)(this.nudKey)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Label lblKey;
+        private System.Windows.Forms.RichTextBox rtbInput;
+        private System.Windows.Forms.NumericUpDown nudKey;
+        private System.Windows.Forms.RichTextBox rtbOutput;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label lblInput;
+        private System.Windows.Forms.Label lblOutput;
+    }
+}
+
---
+++ EncryptionApp/EncryptionApp/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EncryptionApp
+{
+    public partial class frmEcryptapp : Form
+    {
+        public frmEcryptapp()
+        {
+            InitializeComponent();
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            //Default settings
+            rtbOutput.Text = "";// Make sure that the output box is empty
+
+            foreach (char c in rtbInput.Text)
+            {
+                char encrypt = (char)(c + nudKey.Value); //Takes each character inputed it and moves it up by the value the user enters in order to encrypt it 
+                rtbOutput.Text += encrypt.ToString();
+            }
+        }
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            //Default settings
+          //  rtbInput.Text = "";
+
+            foreach (char c in rtbOutput.Text)
+            {
+                char encrypt = (char)(c - nudKey.Value);//Reverse the previous loop 
+                rtbInput.Text += encrypt.ToString();
+            }
+        }
+    private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0); //end application
+        }
+    }
+}
